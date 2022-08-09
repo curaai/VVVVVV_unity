4ae85d63bd9e4d7f8fb59c233fdd31e7f78137e9
curaai <curaai00@gmail.com>
2022-08-09 14:12:19 +0900

    feat: Align coodination left bottom to (0, 0)

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -29,8 +29,8 @@ public class Game : MonoBehaviour
     {
         void AdjustCamPos()
         {
-            var camX = room.pos.x * 640 - 240;
-            var camY = room.pos.y * 480 - 380;
+            var camX = room.pos.x * 640;
+            var camY = room.pos.y * 480 - 40;
 
             cam.localPosition = new Vector3(camX, camY, cam.localPosition.z);
         }
