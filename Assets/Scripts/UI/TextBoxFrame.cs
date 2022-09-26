03ece4f95acc54c2dd54996563041dd607b44b8d
curaai <curaai00@gmail.com>
2022-09-26 17:32:42 +0900

    feat: Set destroy timer in textboxframe
    room: Make new room 'V Stitch' ( 112, 107 )

--- Assets/Scripts/UI/TextBoxFrame.cs
+++ Assets/Scripts/UI/TextBoxFrame.cs
@@ -22,6 +22,8 @@ namespace VVVVVV.UI
 
     public class TextBoxFrame : MonoBehaviour
     {
+        [SerializeField] public float DestroyTimer = -1;
+
         [SerializeField]
         private VVVVVV.UI.Color _color;
         public VVVVVV.UI.Color color
@@ -29,40 +31,49 @@ namespace VVVVVV.UI
             get => _color;
             set
             {
-                UnityEngine.Color32 convert(Color c)
-                {
-                    switch (c)
-                    {
-                        case Color.CYAN:
-                            return new UnityEngine.Color32(164, 164, 255, 255);
-                        case Color.RED:
-                            return new UnityEngine.Color32(255, 60, 60, 255);
-                        case Color.GREEN:
-                            return new UnityEngine.Color32(144, 255, 144, 255);
-                        case Color.YELLOW:
-                            return new UnityEngine.Color32(255, 255, 134, 255);
-                        case Color.BLUE:
-                            return new UnityEngine.Color32(95, 95, 255, 255);
-                        case Color.PURPLE:
-                            return new UnityEngine.Color32(255, 134, 255, 255);
-                        case Color.WHITE:
-                            return new UnityEngine.Color32(244, 244, 244, 255);
-                        case Color.GRAY:
-                            return new UnityEngine.Color32(174, 174, 174, 255);
-                        case Color.ORANGE:
-                            return new UnityEngine.Color32(255, 130, 20, 255);
-                        default:
-                            throw new ArgumentException($"Can't parse color enum {c}");
-                    }
-                }
-
                 _color = value;
-                setColor(convert(color));
+                setColor(color);
             }
         }
 
-        private void setColor(UnityEngine.Color32 c)
+
+        void OnEnable()
         {
+            if (DestroyTimer != -1)
+                Destroy(this, DestroyTimer);
+        }
+
+        private void setColor(Color colorEnum)
+        {
+            UnityEngine.Color32 convert(Color c)
+            {
+                switch (c)
+                {
+                    case Color.CYAN:
+                        return new UnityEngine.Color32(164, 164, 255, 255);
+                    case Color.RED:
+                        return new UnityEngine.Color32(255, 60, 60, 255);
+                    case Color.GREEN:
+                        return new UnityEngine.Color32(144, 255, 144, 255);
+                    case Color.YELLOW:
+                        return new UnityEngine.Color32(255, 255, 134, 255);
+                    case Color.BLUE:
+                        return new UnityEngine.Color32(95, 95, 255, 255);
+                    case Color.PURPLE:
+                        return new UnityEngine.Color32(255, 134, 255, 255);
+                    case Color.WHITE:
+                        return new UnityEngine.Color32(244, 244, 244, 255);
+                    case Color.GRAY:
+                        return new UnityEngine.Color32(174, 174, 174, 255);
+                    case Color.ORANGE:
+                        return new UnityEngine.Color32(255, 130, 20, 255);
+                    default:
+                        throw new ArgumentException($"Can't parse color enum {c}");
+                }
+            }
+
+            var c = convert(colorEnum);
+
             var backgroundColor = new Color32((byte)(c.r / 255), (byte)(c.g / 255), (byte)(c.b / 255), 255);
 
             var textObj = transform.GetChild(1);
@@ -93,7 +104,6 @@ namespace VVVVVV.UI
             color = _color;
             transform.GetChild(1).GetComponent<Text>().text = GetComponent<Text>().text;
         }
-
         private void OnDisable() => GetComponent<Animator>().SetTrigger("OnDisable");
     }
 }
